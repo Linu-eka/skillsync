16d1d399a0815e15202d78f6331368e569054f5b
leka0001 <leka0001@student.monash.edu>
2025-11-16 12:41:27 +1100

    feat: query get steps by goal id

--- Backend/skillsync/src/main/java/com/example/skillsync/controller/GoalController.java
+++ Backend/skillsync/src/main/java/com/example/skillsync/controller/GoalController.java
@@ -7,6 +7,7 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import com.example.skillsync.repository.GoalRepository;
 import com.example.skillsync.model.Goal;
 import java.util.List;
+import com.example.skillsync.model.Step;
 
 @Controller
 public class GoalController {
@@ -24,6 +25,15 @@ public class GoalController {
     @QueryMapping
     public Goal getGoalById(@Argument Long id) {
         return repo.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid goal ID"));
+    }
+
+    @QueryMapping
+    public List<Step> getStepsById(@Argument Long id) {
+        Goal goal = repo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid goal ID"));
+        System.out.println("Fetched steps for goal ID: " + id);
+        System.out.println("Number of steps: " + goal.getSteps().size());
+        return goal.getSteps();
     }
 
     @MutationMapping
